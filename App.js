8b37050c02e5f3ed650883abfeb5ec3b9742a0ff
ssommelet21 <92539141+ssommelet21@users.noreply.github.com>
2022-02-21 17:26:32 +0100

    icones Séb

--- App.js
+++ App.js
@@ -6,13 +6,40 @@ import AccueilScreen from "./screens/Accueil";
 import MusiquesScreen from "./screens/Musiques";
 import FilmsScreen from "./screens/Films";
 import AProposScreen from "./screens/APropos";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Accueil" component={AccueilScreen} />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Musiques") {
+            iconName = focused ? "musical-notes" : "musical-notes-outline";
+          } else if (route.name === "Films") {
+            iconName = focused ? "film" : "film-outline";
+          } else if (route.name === "A propos") {
+            iconName = focused
+              ? "information-circle"
+              : "information-circle-outline";
+          } else if (route.name === "Jeux vidéo") {
+            iconName = focused
+              ? "ios-game-controller"
+              : "ios-game-controller-outline";
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "green",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <Tab.Screen name="Home" component={AccueilScreen} />
       <Tab.Screen name="Musiques" component={MusiquesScreen} />
       <Tab.Screen name="Films" component={FilmsScreen} />
       <Tab.Screen name="A propos" component={AProposScreen} />
