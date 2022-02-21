dfaaffc3a790ba5c341dbb9437a3de62faa7b16f
ssommelet21 <92539141+ssommelet21@users.noreply.github.com>
2022-02-21 12:06:58 +0100

    Card

--- screens/Musiques.js
+++ screens/Musiques.js
@@ -6,11 +6,10 @@ import {
   SectionList,
   SafeAreaView,
   Image,
-  FlatList,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
-const REACT_APP_BACKEND_URL = "https://testfront44.herokuapp.com/api";
+const REACT_APP_BACKEND_URL = "https://greta-bibliotheque-jh.herokuapp.com/api";
 
 const ListItem = ({ item }) => {
   return (
@@ -31,8 +30,6 @@ export default function Musiques() {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
-  console.log(SECTIONS);
-  console.log("et ...");
   console.log(data);
 
   useEffect(() => {
@@ -85,102 +82,6 @@ export default function Musiques() {
     </View>
   );
 }
-
-const SECTIONS = [
-  {
-    title: "Made for you",
-    data: [
-      {
-        key: "1",
-        text: "Item text 1",
-        uri: "https://picsum.photos/id/1/200",
-      },
-      {
-        key: "2",
-        text: "Item text 2",
-        uri: "https://picsum.photos/id/10/200",
-      },
-
-      {
-        key: "3",
-        text: "Item text 3",
-        uri: "https://picsum.photos/id/1002/200",
-      },
-      {
-        key: "4",
-        text: "Item text 4",
-        uri: "https://picsum.photos/id/1006/200",
-      },
-      {
-        key: "5",
-        text: "Item text 5",
-        uri: "https://picsum.photos/id/1008/200",
-      },
-    ],
-  },
-  {
-    title: "Punk and hardcore",
-    data: [
-      {
-        key: "1",
-        text: "Item text 1",
-        uri: "https://picsum.photos/id/1011/200",
-      },
-      {
-        key: "2",
-        text: "Item text 2",
-        uri: "https://picsum.photos/id/1012/200",
-      },
-
-      {
-        key: "3",
-        text: "Item text 3",
-        uri: "https://picsum.photos/id/1013/200",
-      },
-      {
-        key: "4",
-        text: "Item text 4",
-        uri: "https://picsum.photos/id/1015/200",
-      },
-      {
-        key: "5",
-        text: "Item text 5",
-        uri: "https://picsum.photos/id/1016/200",
-      },
-    ],
-  },
-  {
-    title: "Based on your recent listening",
-    data: [
-      {
-        key: "1",
-        text: "Item text 1",
-        uri: "https://picsum.photos/id/1020/200",
-      },
-      {
-        key: "2",
-        text: "Item text 2",
-        uri: "https://picsum.photos/id/1024/200",
-      },
-
-      {
-        key: "3",
-        text: "Item text 3",
-        uri: "https://picsum.photos/id/1027/200",
-      },
-      {
-        key: "4",
-        text: "Item text 4",
-        uri: "https://picsum.photos/id/1035/200",
-      },
-      {
-        key: "5",
-        text: "Item text 5",
-        uri: "https://picsum.photos/id/1038/200",
-      },
-    ],
-  },
-];
 
 const styles = StyleSheet.create({
   container: {
