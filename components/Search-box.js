9de6073af61456f1b6fa845b251c22502cfe04a1
ssommelet21 <92539141+ssommelet21@users.noreply.github.com>
2022-02-21 17:00:32 +0100

    search box

---
+++ components/Search-box.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+
+const SearchBox = (props) => (
+  <View style={styles.container}>
+    <TextInput
+      style={styles.input}
+      placeholder={props.message}
+      onChangeText={props.onSearch}
+    />
+  </View>
+);
+
+export default SearchBox;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#707070",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
--- screens/Musiques.js
+++ screens/Musiques.js
@@ -9,25 +9,52 @@ import {
 import { StatusBar } from "expo-status-bar";
 
 import { FormatSections } from "../includes/Functions_partagees";
+import SearchBox from "../components/Search-box";
 import Card from "../components/Card";
 
 const REACT_APP_BACKEND_URL = "https://greta-bibliotheque-jh.herokuapp.com/api";
 
 export default function Musiques() {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(true); // booléen qui change après un fetch
+  const [data, setData] = useState([]); // toutes les données
+  const [dataSearch, setDataSearch] = useState([]); // les données filtrées via search box
+  const [searchField, setSearchField] = useState(""); // search box
+  //
   const TYPEOFMEDIA = "musiques";
 
   useEffect(() => {
     fetch(REACT_APP_BACKEND_URL + "/" + TYPEOFMEDIA)
       .then((response) => response.json())
-      .then((json) => setData(FormatSections(json[TYPEOFMEDIA], TYPEOFMEDIA)))
+      .then((json) => {
+        let temp_array = FormatSections(json[TYPEOFMEDIA], TYPEOFMEDIA);
+        setData(temp_array); // copie globale
+        setDataSearch(temp_array); // partie filtrée ...
+      })
       .catch((error) => console.error(error))
       .finally(() => setLoading(false));
   }, []);
 
+  useEffect(() => {
+    if (data.length > 0) {
+      var temp_array = [];
+      temp_array = data[0].data.filter((adata) =>
+        adata.text.toLowerCase().includes(searchField.toLowerCase())
+      );
+      var trav = {
+        title: data[0].title,
+        data: temp_array,
+      };
+      setDataSearch([trav]);
+    }
+  }, [searchField]);
+
+  const onSearchChange = (textSearched) => {
+    setSearchField(textSearched);
+  };
+
   return (
     <View style={styles.container}>
+      <SearchBox onSearch={onSearchChange} message="Rechercher un titre" />
       <StatusBar style="light" />
       <SafeAreaView style={{ flex: 1 }}>
         {isLoading ? (
@@ -36,7 +63,7 @@ export default function Musiques() {
           <SectionList
             contentContainerStyle={{ paddingHorizontal: 10 }}
             stickySectionHeadersEnabled={false}
-            sections={data}
+            sections={dataSearch}
             renderSectionHeader={({ section }) => (
               <>
                 <Text style={styles.sectionHeader}>
